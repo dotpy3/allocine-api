158841980486f06ef69eef4c4553e62f4ba269d9
Eric Gourlaouen <gourlaoueneric@gmail.com>
2015-08-15 16:24:02 +0200

    changé gestion header

--- PHP/get.php
+++ PHP/get.php
@@ -1,8 +1,7 @@
 <?php
 
-header('Content-Type: application/json');
-
 require_once(__DIR__.'/allocine.class.php');
+require_once(__DIR__.'/header.php');
 
 define('ALLOCINE_PARTNER_KEY', '100043982026');
 define('ALLOCINE_SECRET_KEY', '29d185d98c984a359e6e6f26a0474269');
