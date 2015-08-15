0e4d6b3da2627e5143ef8a01f40cc87d5930fc40
Eric Gourlaouen <gourlaoueneric@gmail.com>
2015-08-15 16:48:16 +0200

    objet allocine déplacé dans le header

--- PHP/get.php
+++ PHP/get.php
@@ -3,11 +3,6 @@
 require_once(__DIR__.'/allocine.class.php');
 require_once(__DIR__.'/header.php');
 
-define('ALLOCINE_PARTNER_KEY', '100043982026');
-define('ALLOCINE_SECRET_KEY', '29d185d98c984a359e6e6f26a0474269');
-
-$allocine = new Allocine(ALLOCINE_PARTNER_KEY, ALLOCINE_SECRET_KEY);
-
 isset($_GET["movieid"]) ? $result = $allocine->get($_GET["movieid"]) : $result = "{\"movie\": 'noParameter'}";
 
 echo $result;
--- PHP/header.php
+++ PHP/header.php
@@ -1,3 +1,8 @@
 <?php
 
 header('Content-Type: application/json');
+
+define('ALLOCINE_PARTNER_KEY', '100043982026');
+define('ALLOCINE_SECRET_KEY', '29d185d98c984a359e6e6f26a0474269');
+
+$allocine = new Allocine(ALLOCINE_PARTNER_KEY, ALLOCINE_SECRET_KEY);
